5de7e25cd2265976022bcfc6002b917ee14cb486
Dan Lindsay <dlindsay72@hotmail.com>
2017-04-26 13:17:35 -0400

    Album class created, empty Song struct added

---
+++ iTunesSearchClient/Song.swift
@@ -0,0 +1,13 @@
+//
+//  Song.swift
+//  iTunesSearchClient
+//
+//  Created by Dan Lindsay on 2017-04-26.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    
+}
