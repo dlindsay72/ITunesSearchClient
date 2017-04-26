f764031aa622186d99eb00255dc349559527ad7a
Dan Lindsay <dlindsay72@hotmail.com>
2017-04-26 12:39:53 -0400

    Initial Commit

---
+++ iTunesSearchClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesSearchClient
+//
+//  Created by Dan Lindsay on 2017-04-26.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
