378f6448513ed7ebaed8b49e2c873df643e3a63d
Dan Lindsay <dlindsay72@hotmail.com>
2017-04-26 12:52:11 -0400

    Artist, Album classes created, enum for Genre added too

---
+++ iTunesSearchClient/Album.swift
@@ -0,0 +1,13 @@
+//
+//  Album.swift
+//  iTunesSearchClient
+//
+//  Created by Dan Lindsay on 2017-04-26.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class Album {
+    
+}
---
+++ iTunesSearchClient/Artist.swift
@@ -0,0 +1,23 @@
+//
+//  Artist.swift
+//  iTunesSearchClient
+//
+//  Created by Dan Lindsay on 2017-04-26.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class Artist {
+    let id: Int
+    let name: String
+    let primaryGenre: Genre
+    var albums: [Album]
+    
+    init(id: Int, name: String, primaryGenre: Genre, albums: [Album]) {
+        self.id = id
+        self.name = name
+        self.primaryGenre = primaryGenre
+        self.albums = albums
+    }
+}
