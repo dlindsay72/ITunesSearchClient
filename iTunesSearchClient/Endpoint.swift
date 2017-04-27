25ce99100f6aa1c2a5cb48e2f9d5de2364db783c
Dan Lindsay <dlindsay72@hotmail.com>
2017-04-27 15:35:44 -0400

    defined our search endpoint, setup an iTunesMedia enum and extended with CustomStringConvertible

---
+++ iTunesSearchClient/Endpoint.swift
@@ -0,0 +1,77 @@
+//
+//  Endpoint.swift
+//  iTunesSearchClient
+//
+//  Created by Dan Lindsay on 2017-04-27.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+protocol Endpoint {
+    var base: String { get }
+    var path: String { get }
+    var queryItems: [URLQueryItem] { get }
+}
+
+extension Endpoint {
+    var urlComponents: URLComponents {
+        var components = URLComponents(string: base)!
+        components.path = path
+        components.queryItems = queryItems
+        
+        return components
+    }
+    
+    var request: URLRequest {
+        let url = urlComponents.url!
+        return URLRequest(url: url)
+    }
+}
+
+enum Itunes {
+    case search(term: String, media: ItunesMedia?)
+}
+
+extension Itunes: Endpoint {
+    var base: String {
+        return "https://itunes.apple.com"
+    }
+    
+    var path: String {
+        switch self {
+        case .search: return "/search"
+        }
+    }
+    
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case .search(let term, let media):
+            var result = [URLQueryItem]()
+            let searchTermItem = URLQueryItem(name: "term", value: term)
+            result.append(searchTermItem)
+            
+            if let media = media {
+                let mediaItem = URLQueryItem(name: "media", value: media.description)
+                result.append(mediaItem)
+            }
+            
+            return result
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ iTunesSearchClient/iTunesMedia.swift
@@ -0,0 +1,39 @@
+//
+//  iTunesMedia.swift
+//  iTunesSearchClient
+//
+//  Created by Dan Lindsay on 2017-04-27.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+enum ItunesMedia {
+    case movie
+    case podcast
+    case music
+    case musicVideo
+    case audiobook
+    case shortFilm
+    case tvShow
+    case software
+    case ebook
+    case all
+}
+
+extension ItunesMedia: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .movie: return "movie"
+        case .podcast: return "podcast"
+        case .music: return "music"
+        case .musicVideo: return "musicVideo"
+        case .audiobook: return "audiobook"
+        case .shortFilm: return "shortFilm"
+        case .tvShow: return "tvShow"
+        case .software: return "software"
+        case .ebook: return "ebook"
+        case .all: return "all"
+        }
+    }
+}
